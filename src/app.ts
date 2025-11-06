c173a5d202175925a1f58ce0e728070b64b43e38
Matheus-Ribeiro-Jesus <pessoalsenacsp@gmail.com>
2025-11-06 15:25:31 -0300

    Feat|Test: Escrito exemplos de acesso ao conteudo. #4

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,43 @@
 import express from "express";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
+app.use(express.json());
+
+app.get("/query", (req: Request, res: Response, next:NextFunction) =>{
+  const nome = req.query.nome
+  console.log("Cliente digitou: ", nome);
+  res.send(`Você digitou o nome: ${nome}`)
+})
+
+app.get("/parametro/:nome", (req: Request, res: Response, next:NextFunction) =>{
+  const nome = req.params.nome
+  console.log("Rota de parametro - Cliente digitou: ", nome);
+  res.send(`Você digitou o nome: ${nome}`)
+})
+
+app.get("/exemplo", (req: Request, res: Response, next:NextFunction) =>{
+  console.log('aconteceu algo');
+  res.send('rodou tudo certo')
+
+});
+
+
+app.get("/corpo", (req: Request, res: Response, next:NextFunction) =>{
+  const nome = req.body.nome;
+  console.log(nome);
+  res.send(`Variavel dentro do corpo - você digitou o nome:  ${nome}`);
+
+});
+
+
+
+// rota de erro
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err);
+  res.status(500).send("Erro na requisição");
+
+});
+
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-import app from "./app";
-import { Request, Response, NextFunction } from "express";
+import app from "./app"
+
+
 const PORT = 3000;
 
-// rota generica
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-});
-
-// rota de erro
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
-  res.status(500).send("Erro na requisição");
-
-});
-
 app.listen(PORT, () => console.log(`Servidor está rodando na PORTA: ${PORT}`));
+
